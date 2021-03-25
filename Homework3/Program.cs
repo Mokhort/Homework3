7e6a380ab84a6ffffbaeb7dfcca44c230e7f2dda
Mpkhort <giselle98@mail.ru>
2021-03-25 19:41:53 +0300

    Creation of DB "AirStore"

---
+++ Homework3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ Homework3/Route.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Route
+    {
+        public int Id { get; set; }
+        public string Airport_dep { get; set; }
+        public string Airport_arr { get; set; }
+        public int destination { get; set; }
+
+    }
+}
